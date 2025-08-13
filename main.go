adf2bb928939d7eeac991b8c3b944af59b83fb1b
olegmx <oleg.maksimov@gmail.com>
2025-08-13 11:35:27 +0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,75 @@
+/*
+*****************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+******************************************************************************
+*/
+package main
+
+import "fmt"
+import "math/rand"
+
+func makeRandomSlice(numItems, max int) []int {
+	slice := make([]int, numItems)
+	for i := 0; i < numItems; i++ {
+		slice[i] = rand.Intn(max)
+	}
+	return slice
+}
+
+func printSlice(slice []int, numItems int) {
+	if numItems > len(slice) {
+		fmt.Println(slice)
+	} else {
+		fmt.Println(slice[:numItems])
+	}
+}
+
+func checkSorted(slice []int) {
+	for i := 1; i < len(slice); i++ {
+		if slice[i] < slice[i-1] {
+			fmt.Println("Slice is not sorted")
+			return
+		}
+	}
+	fmt.Println("Slice is sorted")
+}
+
+func bubbleSort(slice []int) {
+	swapped := true
+	for swapped {
+		swapped = false
+		for i := 1; i < len(slice); i++ {
+			if slice[i-1] > slice[i] {
+				slice[i], slice[i-1] = slice[i-1], slice[i]
+				swapped = true
+			}
+		}
+	}
+}
+
+func main() {
+	var numItems, max, itemsToPrint int
+	fmt.Printf("# Items: ")
+	fmt.Scanln(&numItems)
+	fmt.Printf("Max: ")
+	fmt.Scanln(&max)
+	fmt.Printf("Items to print: ")
+	fmt.Scanln(&itemsToPrint)
+
+	// Make and display an unsorted slice.
+	slice := makeRandomSlice(numItems, max)
+	printSlice(slice, itemsToPrint)
+	fmt.Println()
+
+	// Sort and display the result.
+	bubbleSort(slice)
+	printSlice(slice, itemsToPrint)
+	printSlice(slice, 1000)
+
+	// Verify that it's sorted.
+	checkSorted(slice)
+}
